f23de13edefd863f044ab7c71798255ff0980b29
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-03-22 00:05:09 +0500

    all is done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,21 @@
-"use client";
-
 import "../style/global.css";
 import ReduxProvider from "@/provider/store.provider";
 import ThemeWrapper from "@/components/ui/ThemeWrapper";
-import { usePathname } from "next/navigation";
-import { AnimatePresence, motion } from "framer-motion";
+import PageTransition from "@/components/layout/PageTransition";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
   return (
     <html lang="en">
       <body>
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={pathname}
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.3 }}
-          >
-            <ReduxProvider>
-              <ThemeWrapper>{children}</ThemeWrapper>
-            </ReduxProvider>
-          </motion.div>
-        </AnimatePresence>
+        <ReduxProvider>
+          <ThemeWrapper>
+            <PageTransition>{children}</PageTransition>
+          </ThemeWrapper>
+        </ReduxProvider>
       </body>
     </html>
   );
