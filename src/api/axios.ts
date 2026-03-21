31c86e998e8c3d0ebac957b6d621bee268bb28ed
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-03-21 14:30:06 +0500

    clear project

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,30 +1,29 @@
 import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
-
-// import { env } from "@/env";
 import { STORAGE_KEYS } from "@/interface/constants";
 
 const api: AxiosInstance = axios.create({
   baseURL: "https://cemare5406.pythonanywhere.com/",
 });
 
-// Request interceptor to add auth token
+// Request interceptor
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem(STORAGE_KEYS.TOKEN);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem(STORAGE_KEYS.TOKEN);
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
     return config;
   },
   (error) => Promise.reject(error),
 );
 
-// Response interceptor for error handling
+// Response interceptor
 api.interceptors.response.use(
   (response: AxiosResponse) => response,
   (error: AxiosError) => {
-    if (error.response?.status === 401) {
-      // Token expired or invalid
+    if (typeof window !== "undefined" && error.response?.status === 401) {
       localStorage.removeItem(STORAGE_KEYS.TOKEN);
       localStorage.removeItem(STORAGE_KEYS.USER);
       window.location.href = "/login";
